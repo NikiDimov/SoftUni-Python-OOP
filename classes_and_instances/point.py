8491121471954f741b81ed510aa2bcbf90d97bab
niki <niki.dimov86@gmail.com>
2021-03-10 20:23:22 +0200

    new

---
+++ classes_and_instances/point.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def set_x(self, new_x):
+        self.x = new_x
+
+    def set_y(self, new_y):
+        self.y = new_y
+
+    def distance(self, x, y):
+        res = pow((self.x - x), 2) + pow((self.y - y), 2)
+        return sqrt(res)
+
+
+p = Point(2, 4)
+p.set_x(3)
+p.set_y(5)
+print(p.distance(10, 2))
