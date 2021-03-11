683fa7b5545ccfe76813037441d4962ddb3ab9af
niki <niki.dimov86@gmail.com>
2021-03-11 18:50:08 +0200

    new

---
+++ classes_and_instances/time.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+
+
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+        self.time_object = datetime(1, 1, 1, hour=hours, minute=minutes, second=seconds)
+
+    def set_time(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+    def next_second(self):
+        self.time_object += timedelta(seconds=1)
+        self.hours = self.time_object.hour
+        self.minutes = self.time_object.minute
+        self.seconds = self.time_object.second
+        return self.get_time()
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+time = Time(23, 59, 59)
+print(time.next_second())
