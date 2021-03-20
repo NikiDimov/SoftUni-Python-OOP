cfee93be8830992f7856bf578c2c5a54ab4b2ee2
niki <niki.dimov86@gmail.com>
2021-03-20 21:15:52 +0200

    new

---
+++ encapsulation/pizza_calories/pizza.py
@@ -0,0 +1,49 @@
+class Pizza:
+    def __init__(self, name, dough, toppings_capacity):
+        self.__name = name
+        self.__dough = dough
+        self.__toppings_capacity = toppings_capacity
+        self.__toppings = {}
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def dough(self):
+        return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        self.__dough = value
+
+    @property
+    def toppings_capacity(self):
+        return self.__toppings_capacity
+
+    @toppings_capacity.setter
+    def toppings_capacity(self, value):
+        self.__toppings_capacity = value
+
+    @property
+    def toppings(self):
+        return self.__toppings
+
+    @toppings.setter
+    def toppings(self, value):
+        self.__toppings = value
+
+    def add_topping(self, topping):
+        if len(self.__toppings) < self.__toppings_capacity:
+            if topping.topping_type not in self.__toppings:
+                self.__toppings[topping.topping_type] = 0
+            self.__toppings[topping.topping_type] += topping.weight
+        else:
+            raise ValueError("Not enough space for another topping")
+
+    def calculate_total_weight(self):
+        return self.__dough.weight + sum(self.__toppings.values())
