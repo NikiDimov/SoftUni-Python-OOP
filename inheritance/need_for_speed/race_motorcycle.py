6e537feff34a7b85efd2f768fd17a1e39e16d3d8
niki <niki.dimov86@gmail.com>
2021-03-26 21:30:19 +0200

    new

---
+++ inheritance/need_for_speed/race_motorcycle.py
@@ -0,0 +1,8 @@
+from OOP.inheritance.need_for_speed.motorcycle import Motorcycle
+
+
+class RaceMotorcycle(Motorcycle):
+    DEFAULT_FUEL_CONSUMPTION = 8.0
+
+
+
---
+++ inheritance/need_for_speed/sport_car.py
@@ -0,0 +1,8 @@
+from OOP.inheritance.need_for_speed.car import Car
+
+
+class SportCar(Car):
+    DEFAULT_FUEL_CONSUMPTION = 10.0
+
+
+
