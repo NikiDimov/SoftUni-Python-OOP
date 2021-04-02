6b9507c5f4e7de04c8fa161751385149dae1e64a
niki <niki.dimov86@gmail.com>
2021-04-02 20:57:49 +0300

    new

---
+++ polymorphism_and_magic_methods/animals/cat.py
@@ -0,0 +1,8 @@
+from OOP.polymorphism_and_magic_methods.animals.animal import Animal
+
+
+class Cat(Animal):
+
+    def make_sound(self):
+        return "Meow meow!"
+
---
+++ polymorphism_and_magic_methods/animals/dog.py
@@ -0,0 +1,8 @@
+from OOP.polymorphism_and_magic_methods.animals.animal import Animal
+
+
+class Dog(Animal):
+
+    def make_sound(self):
+        return "Woof!"
+
