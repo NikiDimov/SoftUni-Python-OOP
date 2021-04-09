3fc200ed6ac10e1ebb61b666234ce242fa2d0ce7
niki <niki.dimov86@gmail.com>
2021-04-09 20:09:06 +0300

    new

---
+++ decorators/execution_time.py
@@ -0,0 +1,32 @@
+from timeit import default_timer as timer
+
+
+def exec_time(func):
+    def wrapper(*args):
+        start_timer = timer()
+        func(*args)
+        return timer() - start_timer
+
+    return wrapper
+
+
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+
+
+print(loop(1, 10000000))
+
+
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+
+
+print(concatenate(["a" for i in range(1000000)]))
