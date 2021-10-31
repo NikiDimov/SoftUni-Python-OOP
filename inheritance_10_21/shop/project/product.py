d1ab9671d61b2a57bb42e2956828a265fabf6ad4
niki <niki.dimov86@gmail.com>
2021-10-31 11:36:05 +0200

    exercise

---
+++ inheritance_10_21/shop/project/product.py
@@ -0,0 +1,13 @@
+class Product:
+    def __init__(self, name, quantity):
+        self.name = name
+        self.quantity = quantity
+
+    def decrease(self, quantity):
+        if self.quantity >= quantity:
+            self.quantity -= quantity
+
+    def increase(self, quantity):
+        self.quantity += quantity
+
+    
---
+++ inheritance_10_21/shop/project/product_repository.py
@@ -0,0 +1,22 @@
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for p in self.products:
+            if p.name == product_name:
+                return p
+
+    def remove(self, product_name):
+        for p in self.products:
+            if p.name == product_name:
+                self.products.remove(p)
+
+    def __repr__(self):
+        result = ''
+        for p in self.products:
+            result += f"{p.name}: {p.quantity}\n"
+        return result.strip()
