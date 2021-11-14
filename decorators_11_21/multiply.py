ef3b36945d79f34f1c650eb77c196fa3aca87810
niki <niki.dimov86@gmail.com>
2021-11-14 20:46:27 +0200

    lab

---
+++ decorators_11_21/multiply.py
@@ -0,0 +1,14 @@
+def multiply(times):
+    def decorator(function):
+        def wrapper(number):
+            return function(number)*times
+        return wrapper
+    return decorator
+
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+
+print(add_ten(6))
